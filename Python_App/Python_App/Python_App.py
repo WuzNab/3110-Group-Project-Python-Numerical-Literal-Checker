7da559e823b28d1d35b376ace4f2da4f5b96ef19
WuzNab <wangv9141@gmail.com>
2025-03-19 22:37:23 -0700

    +classes

--- Python_App/Python_App/Python_App.py
+++ Python_App/Python_App/Python_App.py
@@ -1,1 +1,30 @@
+def main():
+    consoleInput();
+
+def consoleInput():
+    numericLiteral = input("Enter Numerical Literal(s):")
+    
+    
+    
+class FloatingPoints:
+    print()
+
+class IntegerLiterals:
+    def decintegerDigit(numericLiteral: str) -> bool:
+        print()
+    
+    def decintegerNonZeroDigit(numericLiteral: str) -> bool:
+        print()
+
+    def bininteger(numericLiteral: str) -> bool:
+        print()
+    
+    def octinteger(numericLiteral: str) -> bool:
+        print()
+    
+    def hexinteger(numericLiteral: str) -> bool:
+        print()
+    
+
+    
 
