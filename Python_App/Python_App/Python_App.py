978e115665d37172cdcdf2d7a8c571c6037ab7d8
TimothyChristian23 <tdchristian@cpp.edu>
2025-04-02 00:24:07 -0700

    Added floating point literal function
    Added the floating point literal function to the code.

--- Python_App/Python_App/Python_App.py
+++ Python_App/Python_App/Python_App.py
@@ -27,7 +27,10 @@ def hexinteger(numericLiteral: str) -> bool:
     if numericLiteral[0:2].lower() != "0x":
         return False
     
-    hex_part : numericLiteral[2:]
+    hex_part = numericLiteral[2:]
+
+    if not hex_part:
+        return False
 
     if hex_part[-1] == "_":
         return False
@@ -39,8 +42,53 @@ def hexinteger(numericLiteral: str) -> bool:
     if not all(c in '0123456789abcdefABCDEF' for c in cleaned):
         return False
         
-    return true
+    return True
+
+def floatingPoint(numericLiteral: str) -> bool:
+    if not numericLiteral:
+        return False
+
+    if numericLiteral.lower().count('e' > 1) :
+        return False
+
+    if 'e' in numericLiteral or 'E' in numericLiteral:
+        parts  = numericLiteral.split('e') if 'e' in numericLiteral else numericLiteral.split('E')
     
+        if len(parts) != 2:
+            return False
+
+        base, exponent = parts
+
+        if not exponent or exponent[-1] == '_' or '__' in exponent:
+            return False
+
+        cleaned_exponent = exponent.replace('_', '')
+        if not cleaned_exponent.lstrip('-').isdigit():
+            return False
+
+    else :
+        base = numericLiteral
+        if base[-1] == '_' or '__' in base:
+            return False
+
+        if "." not in base:
+            return False
+
+        if base[-1] == "_" or base[-1] ==  ".":
+            return False
+
+        if "__" in base:
+            return False
+
+        cleaned_base = base.replace('_', '')
+        if not all(c in '0123456789.' for c in cleaned_base):
+            return False
+
+        if cleaned_base.count('.') > 1:
+            return False
+
+        return True
+
 
     
 
